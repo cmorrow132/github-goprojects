ed5732a5cb14b0a606ab9ad1638b12cec4312bce
Matt Morrow <cmorrow132@gmail.com>
2016-12-30 00:55:24 -0600

    updated restapi1

--- restapi1/main.go
+++ restapi1/main.go
@@ -1,22 +1,24 @@
 package main
 
 import (
-	"bytes"
+	//"bytes"
 	"database/sql"
 	"fmt"
 	"net/http"
 	"github.com/gin-gonic/gin"
 	_ "github.com/go-sql-driver/mysql"
-	"encoding/json"
+	//"encoding/json"
 )
 
-type Person struct {
-	Id		int
-	First_Name	string	`json:"first_name"`
-	Last_Name	string	`json:"last_name"`	
-}
+
 
 func main() {
+	type Person struct {
+		Id		int
+		First_Name	string	`json:"first_name"`
+		Last_Name	string	`json:"last_name"`
+	}
+
 	db,err:=sql.Open("mysql","goservices:password@tcp(127.0.0.1:3306)/restap1")
 
 	if err!=nil {
@@ -44,7 +46,7 @@ func main() {
 		if err!=nil {
 			result=gin.H{
 				"result": nil,
-				"count:" 0,
+				"count": 0,
 			}
 		} else {
 			result=gin.H{
@@ -54,7 +56,25 @@ func main() {
 		}
 
 		c.JSON(http.StatusOK,result)
-	}
+	})
+
+	router.GET("/persons", func(c *gin.Context) {
+		var (
+			person Person
+			persons []Person
+		)
+
+		rows,err:=db.Query("select id, first_name, last_name from person;")
+		if err!=nil {
+			fmt.Print(err.Error())
+		}
+
+		defer rows.Close()
+		c.JSON(http.StatusOK, gin.H{
+			"result": persons,
+			"count": len(persons),
+		})
+	})
 
 	router.Run(":3000")
 }
