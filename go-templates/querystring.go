42e81182e620d614435eda0f497adde65f2bb902
Matt Morrow <cmorrow132@gmail.com>
2016-12-31 16:52:44 -0600

    latest update

--- go-templates/querystring.go
+++ go-templates/querystring.go
@@ -1,48 +1,98 @@
 package main
 
 import (
-	//"fmt"
+	"fmt"
 	"text/template"
 	"net/http"
-	"io"
+	//"io"
 	"log"
+	_ "github.com/go-sql-driver/mysql"
+	"database/sql"
+	"strings"
 )
 
 type PageTags struct {
 	Title string	`json:"title"`
 	RecordCount int	`json:"recordcount"`
-}
-
-type Person struct {
 	FirstName	string	`json:"firstname"`
 	LastName	string	`json:"lastname"`
 	Phone		string	`json:"phone"`
+	DebugData	string
 }
 
+var (
+	dbFirstName string
+	dbLastName string
+	dbPhone string
+	resultCount int
+	dbResults string
+	pageDebugData string
+)
+
 func pageHandler(w http.ResponseWriter,r *http.Request) {
+
+	db, err := sql.Open("mysql", "goservices:password@/address_book")
+	if err != nil {
+		panic(err.Error())
+	}
+	defer db.Close()
+
+	err=db.Ping()
+	if err != nil {
+		panic(err.Error())
+	}
+
 	//First check the path
 	rURL:=r.URL.Path[1:]	//Strip the leading slash
-
-	if rURL=="" {
-		//Load the main template
-		t,err:=template.ParseFiles("templates/main.tpl")
-		if err!=nil { log.Fatalln(err.Error()) }
-		t.Execute(w,PageTags{Title:"Address Book",})
-
-	}
 
 	if r.Method=="GET" {
 		key:="q"
 		val:=r.URL.Query().Get(key)
-		io.WriteString(w,"<br><br>")
-		io.WriteString(w,"You sent a GET request<br>")
-		io.WriteString(w,"You searched for '" + val + "'")
+		
+		pageDebugData="You sent a GET request<br>You searched for '" + key + "=" + val +"'"
 	} else {
-		io.WriteString(w,"You are posting data\n")
+		pageDebugData="You are posting data<br>"
+		r.ParseForm()
+		for k,v:=range r.Form {
+			pageDebugData+= k+" = "+strings.Join(v,"")+"<br>"
+		}
 	}
+
+	if rURL=="" {
+		//Load the main template
+		tpl:=template.New("main.tpl")
+		tpl=tpl.Funcs(template.FuncMap{
+			"populateData": func() string {
+				rows,err := db.Query("select firstname, lastname, phone from records")
+				defer rows.Close()
+
+				dbResults=""
+				resultCount=0
+				for rows.Next() {
+					err=rows.Scan(&dbFirstName,&dbLastName,&dbPhone)
+					if err!=nil {
+						log.Fatalln(err)
+					}
+					dbResults+="<tr><td>"+dbFirstName+"</td><td>"+dbLastName+"</td><td>"+dbPhone+"</td></tr>"
+					resultCount=resultCount+1
+				}
+
+				return dbResults
+			},
+		})
+
+		tpl,err=tpl.ParseFiles("templates/main.tpl")
+		if err!=nil { log.Fatalln(err.Error()) }
+		err = tpl.Execute(w,PageTags{Title:"Address Book",RecordCount:resultCount,DebugData:pageDebugData,})
+		if err!=nil {
+			log.Fatalln(err)
+		}
+	}
+
 }
 
 func main() {
 	http.HandleFunc("/",pageHandler)
+	fmt.Println("Listening and ready on port :8080")
 	http.ListenAndServe(":8080",nil)
 }
--- go-templates/templates/main.tpl
+++ go-templates/templates/main.tpl
@@ -1,20 +1,46 @@
 <html>
 <head>	
 	<title>{{.Title}}</title>
+
+	<script type="text/javascript" src="https://ajax.googleapis.com/ajax/libs/jquery/3.1.1/jquery.min.js"></script>
+	<script type="text/javascript">
+		$(document).ready(function() {
+			$('#reloadData').click(function() {
+				$('#dbResult').load('/' #main',function() {});
+			});
+		});
+	</script>
 </head>
 <body>
 <center>
 	<h2><u>{{.Title}}</u></h2>
 	<br>
 
-	<table width="80%" border=0>
-	<tr>
-		<td colspan=3 bgcolor="yellow" align=left border=0><font size="2"><b>{{.RecordCount}} Records</b><br></td>
-	</tr><tr>
-		<th align=left><u><font size="3">First Name</th><th align=left><u><font size="3">Last Name</th><th align=left><u><font size="3">Phone</th>
-	</tr><tr>
-	</tr></table>
+	<div id="dbResult">
+		<table width="80%" border=0>
+		<tr>
+		    <form method=POST action="/">
+		        <input type=hidden name='cmd' value='filter'>
+		        <td colspan=3><input type=text name='filter' size=50><input type=submit value='Filter'></td>
+		    </form>
+		</tr><tr>
+			<td colspan=3 bgcolor="#FFFC86" align=left border=0><font size="2"><b>{{.RecordCount}} matched records</b><br></td>
+		</tr><tr>
+			<th align=left><u><font size="3">First Name</th><th align=left><u><font size="3">Last Name</th><th align=left><u><font size="3">Phone</th>
+		{{populateData}}
+		<tr>
+			<form method=POST action="/">
+			    <input type=hidden name='cmd' value='add'>
+				<td><input type=text name='firstname'></td><td><input type=text name='lastname'></td><td><input type=text name="phone"><input type=submit value='Add'></td>
+			</form>
+		</tr></table>
+	</div>
 </center>
+<br>
+	<a><div id="reloadData"><u>Refresh</u></div></a>
+<br><br>
+<h4>Debug data</h4>
+{{.DebugData}}
 </body>
 </html>
 
